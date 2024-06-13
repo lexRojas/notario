9dc6b1709e1b27d4650c3604cea2b616bab86961
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-06-12 19:04:45 -0600

    mejora del caso de no cartular una quincena

--- src/components/TablaIndice.jsx
+++ src/components/TablaIndice.jsx
@@ -19,70 +19,100 @@ export const TablaIndice = (props) => {
           },
         })
         .then((res) => {
+          console.log(res.data);
           setdatos(res.data);
         })
-        .catch((error)=>{
-          alert(error)
+        .catch((error) => {
+          alert(error);
         });
     };
 
     getInfo();
   }, [fechaFinal, fechaInicio]);
 
-  return (
-    <div className="table-contenedor">
-      <table id="tabla-index">
-        <thead className="table-encabezado">
-          <tr>
-            <th className="col-narrow">TOMO</th>
-            <th className="col-narrow">FOL.INICIAL</th>
-            <th className="col-narrow">FOL.FINAL</th>
-            <th className="col-narrow">NUMERO</th>
-            <th className="col-narrow">FECHA</th>
-            <th className="col-narrow">HORA</th>
-            <th className="col-wide">ACTO O CONTRATO</th>
-            <th className="col-wide-xl">PARTES</th>
-          </tr>
-        </thead>
-        <tbody>
-          {datos.map(
-            ({
-              id,
-              lugar,
-              folio_1,
-              pag_1,
-              folio_2,
-              pag_2,
-              fecha,
-              escritura,
-              tomo,
-              partes,
-              hora,
-              minutos,
-              contrato,
-              entero,
-              firmas,
-            }) => (
-              <tr key={escritura}>
-                <td> {tomo} </td>
-                <td> {folio_1 + pag_1.substring(0, 1)} </td>
-                <td> {folio_2 + pag_2.substring(0, 1)} </td>
-                <td> {escritura} </td>
-                <td> {fecha} </td>
-                <td> {hora + ":" + minutos} </td>
-                <td> {contrato} </td>
-                <td> {partes} </td>
-              </tr>
-            )
-          )}
-          <tr>
-            <td  className ='nocartule-title'colSpan={8}> -- NO CARTULE ESTA QUINCENA -- </td>
+  if (datos.length > 0) {
+    return (
+      <div className="table-contenedor">
+        <table id="tabla-index">
+          <thead className="table-encabezado">
+            <tr>
+              <th className="col-narrow">TOMO</th>
+              <th className="col-narrow">FOL.INICIAL</th>
+              <th className="col-narrow">FOL.FINAL</th>
+              <th className="col-narrow">NUMERO</th>
+              <th className="col-narrow">FECHA</th>
+              <th className="col-narrow">HORA</th>
+              <th className="col-wide">ACTO O CONTRATO</th>
+              <th className="col-wide-xl">PARTES</th>
             </tr>
-            <tr>  
-            <td colSpan={8}> Ultima Línea </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+          </thead>
+          <tbody>
+            {datos.map(
+              ({
+                id,
+                lugar,
+                folio_1,
+                pag_1,
+                folio_2,
+                pag_2,
+                fecha,
+                escritura,
+                tomo,
+                partes,
+                hora,
+                minutos,
+                contrato,
+                entero,
+                firmas,
+              }) => (
+                <tr key={escritura}>
+                  <td> {tomo} </td>
+                  <td> {folio_1 + pag_1.substring(0, 1)} </td>
+                  <td> {folio_2 + pag_2.substring(0, 1)} </td>
+                  <td> {escritura} </td>
+                  <td> {fecha} </td>
+                  <td> {hora + ":" + minutos} </td>
+                  <td> {contrato} </td>
+                  <td> {partes} </td>
+                </tr>
+              )
+            )}
+            <tr>
+              <td colSpan={8}> Ultima Línea </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    return (
+      <div className="table-contenedor">
+        <table id="tabla-index">
+          <thead className="table-encabezado">
+            <tr>
+              <th className="col-narrow">TOMO</th>
+              <th className="col-narrow">FOL.INICIAL</th>
+              <th className="col-narrow">FOL.FINAL</th>
+              <th className="col-narrow">NUMERO</th>
+              <th className="col-narrow">FECHA</th>
+              <th className="col-narrow">HORA</th>
+              <th className="col-wide">ACTO O CONTRATO</th>
+              <th className="col-wide-xl">PARTES</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="nocartule-title" colSpan={8}>
+                {" "}
+                -- NO CARTULE ESTA QUINCENA --{" "}
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={8}> Ultima Línea </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 };
