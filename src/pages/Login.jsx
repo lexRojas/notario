a6ca6974c3751b36248b49c2156f3e13201ccadc
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-09-26 08:24:04 -0600

    mejoras

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,73 +1,46 @@
-import React, { useState,useRef,useEffect } from "react";
+import React, { useRef } from "react";
 import logo from "..//images/logo.png";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
 
-import { useDispatch } from 'react-redux'
-import { setUser } from '../store/store'
-
+import { useDispatch } from "react-redux";
+import { setUser } from "../store/store";
 
 export const Login = () => {
- 
-  const dispatch  = useDispatch()
+  const dispatch = useDispatch();
 
-
-
-  const navegar = useNavigate();  
-  const [clave, setClave] = useState(null)  
+  const navegar = useNavigate();
 
   const user = useRef();
   const pass = useRef();
 
-  const  get_usuario = async (login) => {
-
+  const get_usuario = async (login, password) => {
     await axios
       .get("https://mysql-backend-8bc5e268b39e.herokuapp.com/users_login", {
         params: { idlogin: login },
       })
       .then((res) => {
+        if (res.data) {
+          dispatch(setUser(res.login));
 
-
-        if (res.data){
-            setClave(res.data.password)
-            dispatch(setUser(res.login))
-        }else{
-            setClave(null)
+          if (res.data.password === password) {
+            navegar("/menu");
+          } else {
+            navegar("/error");
+          }
+        } else {
+          navegar("/error");
         }
       })
-      .catch( (e)=>{
-
-        console.log(e)
-      }
-
-
-      );
+      .catch((e) => {
+        console.log(e);
+        navegar("/error");
+      });
   };
 
-
-
-  const irMenu=async()=>{
-    await get_usuario(user.current.value)
-  }
-
-
-  useEffect(() => {
-
-
-    if (clave){
-
-      if (clave === pass.current.value){
-          navegar("/menu")
-      }else{
-          navegar("/error")
-      }
-
-  }  
-    
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [clave])
-  
-
+  const irMenu = async () => {
+    await get_usuario(user.current.value, pass.current.value);
+  };
 
   return (
     <div className="contenedor">
